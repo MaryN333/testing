c8567213d1e4bc2863d88f3ffb86397692a73b66
MaryN333 <sidyakina@gmail.com>
2024-12-04 14:06:04 +0100

    Initial commit

---
+++ src/test/java/cz/wz/marysidy/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.wz.marysidy.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
